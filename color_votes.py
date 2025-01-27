55dcb4f2ae4dec6b9896f8115d518814fdabbc43
Tecbot <technic.robot.35@gmail.com>
2025-01-26 22:41:01 -0600

    Add heatmap over time, altenate data stora and screenshot folder

--- color_votes.py
+++ color_votes.py
@@ -46,9 +46,11 @@ def group_timeseries(df):
     gp = pd.Grouper(key='date', freq='MS')
     gp_df = df.groupby(gp)
     votes_over_time = gp_df[votes].sum()
-    
     #print(votes_over_time.loc['2024-01-1':'2024-12-30'])
     #print(type(votes_over_time.index))
+    return votes_over_time
+    
+def make_timeseries(votes_over_time):
     fig, ax = plt.subplots(
             figsize = (30, 9))
     fig.autofmt_xdate()
@@ -65,19 +67,65 @@ def group_timeseries(df):
     ax.tick_params(axis='x', rotation=80)
     return fig
 
+def make_heatmap(heatmap, dates, places):
+    x, y = np.meshgrid(places, dates)
+    
+    fig, ax = plt.subplots(figsize = (9, 9))
+    fig.autofmt_xdate()
+    plot = ax.pcolormesh(x, y, heatmap, cmap='plasma')
+
+    ax.set_title("Votes histogram over time")
+    fig.colorbar(plot)
+
+    return fig
+
+def get_heatmap_data(df):
+    max_place = 5
+    slc_df = df.loc[:,['date','place', votes]]
+    # Remove runner ups
+    top_bool = slc_df['place'].astype(str).str.isdigit()
+    slc_df = slc_df[top_bool] 
+    # Remove ties/duplicates
+    slc_df.drop_duplicates(inplace=True)
+    uniq_dates = pd.unique(df['date'])
+    heatmap_array = []
+    placement = [0] * (max_place)
+    curr_date = uniq_dates[0]
+    for index,  row in slc_df.iterrows():
+        place = row['place']
+        count = row[votes]
+        date = row['date']
+        placement[place-1]=count
+        if place == max_place:
+        # if curr_date != date:
+            print(placement, date, place)
+            heatmap_array.append(placement)
+            placement = [0] * max_place
+            curr_date = date
+
+    places = list(range(1, max_place+1))
+    #     print(heatmap_array)
+    np_heatmap = np.array(heatmap_array)
+    return heatmap_array, uniq_dates, places
+
 
 if __name__=="__main__":
     plt.style.use('seaborn-v0_8-darkgrid')
     args = proc_opts()
     twk_df = read_file(args.filename) 
+    time_ser = group_timeseries(twk_df)
+
+    heatmap, dates, places = get_heatmap_data(twk_df)
+    heat_map_fig = make_heatmap(heatmap, dates, places)
 
     hist_fig = make_histogram(twk_df)
-    time_fig = group_timeseries(twk_df)
+    time_fig = make_timeseries(time_ser)
 
     if args.dry_run:
         sys.exit(0)
     if args.graph_out:
         hist_fig.savefig(args.graph_out+"histogram.png")
         time_fig.savefig(args.graph_out+"timeseries.png")
+        heat_map_fig.savefig(args.graph_out+"heatmap.png")
     else:
         plt.show()
